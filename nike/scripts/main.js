8ccd712afa151f904a1e109f3f30ea27fd3f3624
Irakliy69 <77845774+Irakliy69@users.noreply.github.com>
2023-01-12 19:45:54 +0400

    Add files via upload

---
+++ nike/scripts/main.js
@@ -0,0 +1,36 @@
+$('.prod1').on('mouseover', function () {
+    $('.prod1-image').attr('src', 'prod1_hover.png');
+  });
+  
+  $('.prod1').on('mouseout', function () {
+    $('.prod1-image').attr('src', 'prod1_start.png');
+  });
+
+
+$('.prod2').on('mouseover', function () {
+    $('.prod2-image').attr('src', 'prod2_hover.png');
+  });
+  
+  $('.prod2').on('mouseout', function () {
+    $('.prod2-image').attr('src', 'prod2_start.png');
+  });
+
+
+
+$('.prod3').on('mouseover', function () {
+    $('.prod3-image').attr('src', 'prod3_hover.png');
+  });
+  
+  $('.prod3').on('mouseout', function () {
+    $('.prod3-image').attr('src', 'prod3_start.png');
+  });
+  
+
+
+$('.prod4').on('mouseover', function () {
+    $('.prod4-image').attr('src', 'prod4_hover.png');
+  });
+  
+  $('.prod4').on('mouseout', function () {
+    $('.prod4-image').attr('src', 'prod4_start.png');
+  });
